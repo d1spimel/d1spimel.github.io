e292cd57c151d6db368f8755bbd0d4770b0dff62
d1spimel <73076512+d1spimel@users.noreply.github.com>
2022-09-20 15:30:10 +0300

    Add files via upload

--- 8laboratory/script.js
+++ 8laboratory/script.js
@@ -12,13 +12,14 @@
     }
     let name_element = document.getElementById("name");
     name_element.innerHTML = name;
-
+    let new_game = 0;
     function generate()
     {
         let ball_player_element = document.getElementById("ball_player");
         let score_player_element = document.getElementById("score_player");
         let score_computer_element = document.getElementById("score_computer");
         let ball_computer_element = document.getElementById("ball_computer");
+
         let ball_player = getRandomInt(7);
         ball_player_element.innerHTML = ball_player;
         let ball_computer = getRandomInt(7);
@@ -36,12 +37,15 @@
         if (score_player == 3)
         {
             alert(`${name}, you win!`);
+            new_game = 1;
         }
         else if (score_computer == 3)
         {
             alert(`${name}, you lose!`);
+            new_game = 1;
         }
-        if (score_player == 3 || score_computer == 3)
+        new_game++;
+        if (new_game == 2 || score_computer == 2)
         {
             score_player = 0;
             score_player_element.innerHTML = score_player;
