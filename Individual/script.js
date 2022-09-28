fb6f505b3d44ffd5f1b795328ba1fe16bf46d12d
d1spimel <73076512+d1spimel@users.noreply.github.com>
2022-09-28 14:05:37 +0300

    Add files via upload

---
+++ Individual/script.js
@@ -0,0 +1,7 @@
+let wrapper_element = document.getElementById("wrapper");
+wrapper_element.style.opacity = 1;
+for (let i = 1; i <= 3; i++)
+{
+	let game_info_element = document.getElementById("game_info" + i);
+	game_info_element.style.opacity = 1;
+}
